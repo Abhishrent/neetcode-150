a4d5f6e439168289a761fa014d42bce2d9bacab6
abhishrent <abhishrent+github@gmail.com>
2026-05-22 13:01:09 +0545

    add two sum

---
+++ 1.two-sum.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const myMap = new Map()
+    for(let i = 0; i < nums.length; i++) {
+        const partnerNum = target - nums[i]
+        if(myMap.has(partnerNum)){
+            return [myMap.get(partnerNum), i]
+        } else {
+            myMap.set(nums[i], i)
+        }
+    }
+};
+// @lc code=end
+
